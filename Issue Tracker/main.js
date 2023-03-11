a589eca2bd06baceb2c262b8e38e4aed9ee62654
ZAHIRULISLAM-CSE <neloy1613@gmail.com>
2023-03-11 15:59:16 +0600

    solved issue tracker

--- Issue Tracker/main.js
+++ Issue Tracker/main.js
@@ -23,16 +23,19 @@ function submitIssue(e) {
 
 const closeIssue = id => {
   const issues = JSON.parse(localStorage.getItem('issues'));
-  const currentIssue = issues.find(issue => issue.id === id);
+  console.log(issues);
+  const currentIssue = issues.find(issue => issue.id == id);
   currentIssue.status = 'Closed';
+  //console.log(currentIssue);
   localStorage.setItem('issues', JSON.stringify(issues));
   fetchIssues();
 }
 
 const deleteIssue = id => {
   const issues = JSON.parse(localStorage.getItem('issues'));
-  const remainingIssues = issues.filter( issue.id !== id )
+  const remainingIssues = issues.filter( issue=>issue.id != id)
   localStorage.setItem('issues', JSON.stringify(remainingIssues));
+  fetchIssues();
 }
 
 const fetchIssues = () => {
@@ -40,17 +43,21 @@ const fetchIssues = () => {
   const issuesList = document.getElementById('issuesList');
   issuesList.innerHTML = '';
 
-  for (var i = 0; i < issues.length; i++) {
-    const {id, description, severity, assignedTo, status} = issues[i];
-
-    issuesList.innerHTML +=   `<div class="well">
-                              <h6>Issue ID: ${id} </h6>
-                              <p><span class="label label-info"> ${status} </span></p>
-                              <h3> ${description} </h3>
-                              <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
-                              <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
-                              <a href="#" onclick="setStatusClosed(${id})" class="btn btn-warning">Close</a>
-                              <a href="#" onclick="deleteIssue(${id})" class="btn btn-danger">Delete</a>
-                              </div>`;
-  }
+    try{
+      for (let i = 0; i < issues.length; i++) {
+        const { id ,description, severity, assignedTo, status} = issues[i];
+        issuesList.innerHTML +=   `<div class="well">
+                                  <h6>Issue ID: ${id} </h6>
+                                  <p><span class="label label-info"> ${status} </span></p>
+                                  <h3> ${description} </h3>
+                                  <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
+                                  <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
+                                  <a href="#" onclick="closeIssue(${id})" class="btn btn-warning">Close</a>
+                                  <a href="#" onclick="deleteIssue(${id})" class="btn btn-danger">Delete</a>
+                                  </div>`;
+      }
+    }
+    catch(err){
+    }
+  
 }
